7cb55daf4cfe98f6a49b365d7b87f4d0eb0f1b9b
cheali-martins <chealimartins@gmail.com>
2024-06-07 15:45:25 +0100

    new

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Link, NavLink, useLocation, useParams } from "react-router-dom";
-import { NavigationArray } from "./headerlinks";
 import { MdMenu } from "react-icons/md";
-
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { NavigationArray } from "./headerlinks";
 const Header = () => {
+
 
   const pathname = useParams();
   const pathname2 = useLocation();
@@ -11,6 +12,7 @@ const Header = () => {
   console.log(" the pathname from params => ", pathname);
   console.log(" the pathname from params again => ", pathname2.pathname);
 
+  // state for mobile header
   const [openHeader, setOpenHeader] = useState(false);
 
   return (
@@ -21,7 +23,6 @@ const Header = () => {
         <nav className="lg:flex items-center space-x-3 hidden">
           {NavigationArray?.map((navigation) => (
             <Link
-              target="_blank"
               key={navigation.id}
               to={`${navigation.link}`}
               className={`${pathname2?.pathname === navigation?.link ? "text-red-600" : ""
@@ -30,20 +31,62 @@ const Header = () => {
               {navigation.label}
             </Link>
           ))}
+
+          {/* {NavigationArray?.map((navigation) => (
+            <NavLink
+              key={navigation.label}
+              to={`${navigation.link}`}
+              className={({ isActive }) => (isActive ? "text-red-600" : "")}
+            >
+              {navigation.label}
+            </NavLink>
+          ))} */}
+
+          {/* see the difference  */}
+          {/* <NavLink
+            to="about"
+            className={({ isActive }) => (isActive ? "text-red-600" : "")}
+          >
+            About
+          </NavLink> */}
+          {/* <Link to="/about">About</Link> */}
+          {/* <Link to="/blog">Blog</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/contact">Contact</Link> */}
         </nav>
 
         {/* mobile view */}
-
         <nav className="lg:hidden">
-          <MdMenu className="text-red-600 w-10 h-10 cursor-pointer" onClick={() => setOpenHeader(current => !current)} />
+          <MdMenu
+            className="text-red-600 w-10 h-10 cursor-pointer"
+            onClick={() => setOpenHeader(true)}
+          />
           {openHeader && (
             <div className="absolute w-full inset-0 bg-black/70 z-50">
-              <div className="bg-white">mobile modal open</div>
-              <h4>
-                mobile header
-              </h4>
-              <div>
+              <div className="bg-white">
+                <div className="flex items-center justify-between px-5">
+                  <h4>Mobile Header</h4>
+                  <IoCloseCircleOutline
+                    className="w-10 h-10 text-orange-400 cursor-pointer"
+                    onClick={() => setOpenHeader(false)}
+                  />
+                </div>
 
+                <div className="flex flex-col space-y-3 px-3 py-2">
+                  {NavigationArray?.map((navigation) => (
+                    <Link
+                      key={navigation.id}
+                      to={`${navigation.link}`}
+                      className={`${pathname2?.pathname === navigation?.link
+                        ? "text-red-600"
+                        : ""
+                        }`}
+                      onClick={() => setOpenHeader(false)}
+                    >
+                      {navigation.label}
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
           )}
@@ -54,28 +97,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-{/* {NavigationArray?.map((navigation) => (
-            <NavLink
-              key={navigation.label}
-              to={`${navigation.link}`}
-              className={({ isActive }) => (isActive ? "text-red-600" : "")}
-            >
-              {navigation.label}
-            </NavLink>
-          ))} */}
-
-{/* see the difference  */ }
-{/* <NavLink
-            to="about"
-            className={({ isActive }) => (isActive ? "text-red-600" : "")}
-          >
-            About
-          </NavLink> */}
-{/* <Link to="/about">About</Link> */ }
-{/* <Link to="/blog">Blog</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/contact">Contact</Link> */}
-
